d403f765fcdf2491bf82fe0b6e38fdc61acadff6
Andrei Fidelman <andrei.fidelman@actum.cz>
2017-07-31 16:24:24 +0200

    create echo server

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
+// 127.0.0.1/echo?message=Hello -> Hello
 const http = require('http');
+const url = require('url');
 
 const server = new http.Server();
 
 server.listen(1337, '127.0.0.1');
 
 server.on('request', (req, res) => {
-  res.end('Hello world');
+  const urlParsed = url.parse(req.url, true);
+
+  if (urlParsed.pathname === '/echo' && urlParsed.query.message) {
+    res.end(urlParsed.query.message);
+  } else {
+    res.statusCode = 404;
+    res.end('Page not found');
+  }
 });
